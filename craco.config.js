16ad35b6f379c428630a6619a4a31797a99c30c3
wangjiliang <wang_jiliang@outlook.com>
2020-06-08 11:05:55 +0800

    修改配置

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,19 @@
 
+/**
+ * antd 按需加载配置步骤
+ * 1. 安装 antd @craco/craco craco-antd
+ * 2. 修改package.json 文件：
+ * "scripts": {
+    -   "start": "react-scripts start",
+    -   "build": "react-scripts build",
+    -   "test": "react-scripts test",
+    +   "start": "craco start",
+    +   "build": "craco build",
+    +   "test": "craco test",
+    }
+    3. 在项目根目录下创建craco.config.js文件
+ * 
+ */
 
 const CracoAntDesignPlugin = require("craco-antd");
 const CracoLessPlugin = require('craco-less');
