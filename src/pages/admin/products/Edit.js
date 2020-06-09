780cecc6928a3cf821b5871bf96bf8b80dd5791e
wangjiliang <wang_jiliang@outlook.com>
2020-06-09 14:20:42 +0800

    完善添加页面

--- src/pages/admin/products/Edit.js
+++ src/pages/admin/products/Edit.js
@@ -3,7 +3,8 @@ import { Form, Input, Card, Button, message } from 'antd'
 
 function Edit() {
   //表单提交成功的执行函数
-  const onFinish = () => {
+  const onFinish = (values) => {
+    console.log(values)
     message.success('保存成功！')
   }
   //表单提交失败的执行函数
@@ -13,10 +14,20 @@ function Edit() {
   return (
     <Card title='商品编辑'>
       <Form onFinish={onFinish}   onFinishFailed={onFinishFailed} >
-        <Form.Item colon='False' label='商品名称' name='productname' rules={[{ required: true, message: '请输入商品名字!' }]}>
+        <Form.Item label='商品名称' name='productname' rules={[ { required: true, message: '请输入商品名字!' } ]} >
           <Input placeholder='请输入商品名字' />
         </Form.Item>
-        <Form.Item><Button type="primary" htmlType="submit"  >保存</Button></Form.Item>
+        <Form.Item  label='商品价格' name='price'
+          rules={[
+            { required: true, message: '请输入商品价格!' },
+            { validator:(_, value) => value < 100 ? Promise.resolve() : Promise.reject('商品价格不能大于100') }
+          ]}
+        >
+          <Input placeholder='请输入商品价格' />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit"  >保存</Button>
+        </Form.Item>
       </Form>
     </Card>
   )
